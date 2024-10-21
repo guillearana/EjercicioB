6c413f3400d63ab36a5376308a0977875308eb1c
Guillermo <guillermo.arana1@gmail.com>
2024-10-21 10:47:36 +0200

    Target incluido

---
+++ target/reports/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"es.guillearana.ejerciciob","l":"es.guillearana.ejerciciob"}];updateSearchResults();
---
+++ target/reports/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"es.guillearana.ejerciciob","l":"EjercicioBcontroller"},{"p":"es.guillearana.ejerciciob","l":"GestionPersonas"}];updateSearchResults();
